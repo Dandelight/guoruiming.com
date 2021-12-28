1cd827be5a2fb71f09be9bdfaf35af443af536f7
guoruiming <guoruiming@stu.scu.edu.cn>
2021-12-28 11:44:03 +0800

    Add metadata theme-color

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 source ~/.setupvars.sh
 cd $BLOG_ROOT && git fetch && git rebase && git push && source venv/bin/activate && mkdocs build
+python add_meta.py
 # ./scripts/image_resize.sh
---
+++ scripts/add_meta.py
@@ -0,0 +1,26 @@
+"""
+Inplementation of theme-color accroding to: https://developers.google.com/web/updates/2014/11/Support-for-theme-color-in-Chrome-39-for-Android?hl=en
+"""
+
+import os
+
+def add_theme(file, value):
+    print("processing {}".format(file))
+    with open(file, "r", encoding="utf-8") as f:
+        html = f.readlines()
+        for idx, line in enumerate(html):
+            if(line.strip() == "<head>"):
+                target_idx = idx+1
+        html.insert(target_idx, '<meta name="theme-color" content="{}"/>'.format(value))
+    # print(html)
+    with open(file, "w", encoding="utf-8") as f:
+        f.writelines(html)
+
+
+
+
+for root, dirs, files in os.walk("./site"):
+    for file in files:
+        if(file.endswith("html")):
+            add_theme(os.path.join(root, file), "#ffab8f")
+            # exit(0)
