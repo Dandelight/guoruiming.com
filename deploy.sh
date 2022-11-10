d18331be72bc909cf9ff25557ad8ed82cde2c8f6
guoruiming <guoruiming@stu.scu.edu.cn>
2022-11-10 10:21:21 +0800

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 source ~/.setupvars.sh
 cd $BLOG_ROOT && git fetch && git rebase && git push && source venv/bin/activate && mkdocs build &&\
-        python ./scripts/add_meta.py
+        python ./scripts/add_meta.py && npx tcb hosting deploy ./site -e blog-7gq1v71gdf1f8c5a
 # ./scripts/image_resize.sh
+
