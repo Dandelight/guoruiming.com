bd034aa51479529ef85fc51627e91e333d141994
Ruiming Guo <guoruiming@stu.scu.edu.cn>
2024-07-31 01:03:59 +0800

    chore: update dependencies

--- upload.sh
+++ upload.sh
@@ -1,6 +1,5 @@
 # if no coscmd command, prompt the user to install it
-if ! command -v coscmd &> /dev/null
-then
+if ! command -v coscmd &>/dev/null; then
     echo "coscmd could not be found"
     echo "Please run: pip install coscmd"
     exit
